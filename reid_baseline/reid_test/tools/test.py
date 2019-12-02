033e276238ba9efa8100a29596999e14d9538184
bochuan wu <wubochuan@xdf.cn>
2019-12-02 14:46:24 +0800

    update weight and predict function

--- reid_baseline/reid_test/tools/test.py
+++ reid_baseline/reid_test/tools/test.py
@@ -1,14 +1,14 @@
 # encoding: utf-8
 """
-@author:  wubochuan
-@contact: 1670306646@qq.com
+@author:  sherlock
+@contact: sherlockliao01@gmail.com
 """
 
 import argparse
 import os
 import sys
 from os import mkdir
-
+from sklearn.preprocessing import normalize
 import torch
 from torch.backends import cudnn
 
@@ -24,72 +24,79 @@ import json
 from scipy.spatial.distance import cdist
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-re_rank = False
-def k_reciprocal_neigh( initial_rank, i, k1):
-    forward_k_neigh_index = initial_rank[i,:k1+1]
-    backward_k_neigh_index = initial_rank[forward_k_neigh_index,:k1+1]
-    fi = np.where(backward_k_neigh_index==i)[0]
-    return forward_k_neigh_index[fi]
+re_rank = True
 
-def re_ranking(q_g_dist, q_q_dist, g_g_dist, k1=20, k2=6, lambda_value=0.3):
-    # The following naming, e.g. gallery_num, is different from outer scope.
-    # Don't care about it.
-    original_dist = np.concatenate(
-      [np.concatenate([q_q_dist, q_g_dist], axis=1),
-       np.concatenate([q_g_dist.T, g_g_dist], axis=1)],
-      axis=0)
-    original_dist = 2. - 2 * original_dist   #np.power(original_dist, 2).astype(np.float32)
-    original_dist = np.transpose(1. * original_dist/np.max(original_dist,axis = 0))
-    V = np.zeros_like(original_dist).astype(np.float32)
-    #initial_rank = np.argsort(original_dist).astype(np.int32)
-    # top K1+1
-    initial_rank = np.argpartition( original_dist, range(1,k1+1) )
+def re_ranking(probFea, galFea, k1, k2, lambda_value, local_distmat=None, only_local=False):
+    # if feature vector is numpy, you should use 'torch.tensor' transform it to tensor
+    query_num = probFea.size(0)
+    all_num = query_num + galFea.size(0)
+    if only_local:
+        original_dist = local_distmat
+    else:
+        feat = torch.cat([probFea,galFea])
+        print('using GPU to compute original distance')
+        distmat = torch.pow(feat,2).sum(dim=1, keepdim=True).expand(all_num,all_num) + \
+                      torch.pow(feat, 2).sum(dim=1, keepdim=True).expand(all_num, all_num).t()
+        distmat.addmm_(1,-2,feat,feat.t())
+        original_dist = distmat.cpu().numpy()
+        del feat
+        if not local_distmat is None:
+            original_dist = original_dist + local_distmat
+    gallery_num = original_dist.shape[0]
+    original_dist = np.transpose(original_dist / np.max(original_dist, axis=0))
+    V = np.zeros_like(original_dist).astype(np.float16)
+    initial_rank = np.argsort(original_dist).astype(np.int32)
 
-    query_num = q_g_dist.shape[0]
-    all_num = original_dist.shape[0]
-
+    print('starting re_ranking')
     for i in range(all_num):
         # k-reciprocal neighbors
-        k_reciprocal_index = k_reciprocal_neigh( initial_rank, i, k1)
+        forward_k_neigh_index = initial_rank[i, :k1 + 1]
+        backward_k_neigh_index = initial_rank[forward_k_neigh_index, :k1 + 1]
+        fi = np.where(backward_k_neigh_index == i)[0]
+        k_reciprocal_index = forward_k_neigh_index[fi]
         k_reciprocal_expansion_index = k_reciprocal_index
         for j in range(len(k_reciprocal_index)):
             candidate = k_reciprocal_index[j]
-            candidate_k_reciprocal_index = k_reciprocal_neigh( initial_rank, candidate, int(np.around(k1/2)))
-            if len(np.intersect1d(candidate_k_reciprocal_index,k_reciprocal_index))> 2./3*len(candidate_k_reciprocal_index):
-                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index,candidate_k_reciprocal_index)
+            candidate_forward_k_neigh_index = initial_rank[candidate, :int(np.around(k1 / 2)) + 1]
+            candidate_backward_k_neigh_index = initial_rank[candidate_forward_k_neigh_index,
+                                               :int(np.around(k1 / 2)) + 1]
+            fi_candidate = np.where(candidate_backward_k_neigh_index == candidate)[0]
+            candidate_k_reciprocal_index = candidate_forward_k_neigh_index[fi_candidate]
+            if len(np.intersect1d(candidate_k_reciprocal_index, k_reciprocal_index)) > 2 / 3 * len(
+                    candidate_k_reciprocal_index):
+                k_reciprocal_expansion_index = np.append(k_reciprocal_expansion_index, candidate_k_reciprocal_index)
 
         k_reciprocal_expansion_index = np.unique(k_reciprocal_expansion_index)
-        weight = np.exp(-original_dist[i,k_reciprocal_expansion_index])
-        V[i,k_reciprocal_expansion_index] = 1.*weight/np.sum(weight)
-
-    original_dist = original_dist[:query_num,]
+        weight = np.exp(-original_dist[i, k_reciprocal_expansion_index])
+        V[i, k_reciprocal_expansion_index] = weight / np.sum(weight)
+    original_dist = original_dist[:query_num, ]
     if k2 != 1:
-        V_qe = np.zeros_like(V,dtype=np.float32)
+        V_qe = np.zeros_like(V, dtype=np.float16)
         for i in range(all_num):
-            V_qe[i,:] = np.mean(V[initial_rank[i,:k2],:],axis=0)
+            V_qe[i, :] = np.mean(V[initial_rank[i, :k2], :], axis=0)
         V = V_qe
         del V_qe
     del initial_rank
     invIndex = []
-    for i in range(all_num):
-        invIndex.append(np.where(V[:,i] != 0)[0])
+    for i in range(gallery_num):
+        invIndex.append(np.where(V[:, i] != 0)[0])
 
-    jaccard_dist = np.zeros_like(original_dist,dtype = np.float32)
+    jaccard_dist = np.zeros_like(original_dist, dtype=np.float16)
 
     for i in range(query_num):
-        temp_min = np.zeros(shape=[1,all_num],dtype=np.float32)
-        indNonZero = np.where(V[i,:] != 0)[0]
-        indImages = []
+        temp_min = np.zeros(shape=[1, gallery_num], dtype=np.float16)
+        indNonZero = np.where(V[i, :] != 0)[0]
         indImages = [invIndex[ind] for ind in indNonZero]
         for j in range(len(indNonZero)):
-            temp_min[0,indImages[j]] = temp_min[0,indImages[j]]+ np.minimum(V[i,indNonZero[j]],V[indImages[j],indNonZero[j]])
-        jaccard_dist[i] = 1-temp_min/(2.-temp_min)
+            temp_min[0, indImages[j]] = temp_min[0, indImages[j]] + np.minimum(V[i, indNonZero[j]],
+                                                                               V[indImages[j], indNonZero[j]])
+        jaccard_dist[i] = 1 - temp_min / (2 - temp_min)
 
-    final_dist = jaccard_dist*(1-lambda_value) + original_dist*lambda_value
+    final_dist = jaccard_dist * (1 - lambda_value) + original_dist * lambda_value
     del original_dist
     del V
     del jaccard_dist
-    final_dist = final_dist[:query_num,query_num:]
+    final_dist = final_dist[:query_num, query_num:]
     return final_dist
 
 def euclidean_distance(qf,gf):
@@ -180,6 +187,7 @@ def main():
 
     query_dataset, test_dataset, query_loader, test_loader = make_data_loader(cfg)
     model = build_model(cfg, 4768)
+
     model.load_param(cfg.TEST.WEIGHT)
 
     model.to(DEVICE)
@@ -191,29 +199,40 @@ def main():
                                 for img, pid, camid in query_loader])
         test = np.concatenate([model(img.to(DEVICE)).detach().cpu().numpy()
                                for img, pid, camid in test_loader])
-        
+
     query_ids = [pid1 for _, pid1, _ in query_loader]
     queryid = []
     for qid in query_ids:
         qid = list(qid)
         queryid=queryid + qid
     test_ids = [pid2 for _, pid2, _ in test_loader]
-    #print(test_ids)
-    #print(len(test_ids))
+
     testid = []
     for tid in test_ids:
         tid=list(tid)
         testid = testid + tid
-    print(query.shape)
-    #print(test.shape)
-    #print(len(test))
+    feat_norm = cfg.TEST.FEAT_NORM
+    if feat_norm == 'yes':
+        print("The test feature is normalized")
+        feat = np.concatenate((query,test))
+        feat = torch.from_numpy(feat)
+        feat = torch.nn.functional.normalize(feat, dim=1, p=2).numpy()  # along channel
+        query = feat[:3147]
+        test = feat[3147:]
+
+    print(len(query))
     if re_rank:
-        q_g_dist = np.dot(query, np.transpose(test))
-        q_q_dist = np.dot(query, np.transpose(query))
-        g_g_dist = np.dot(test, np.transpose(test))
-        dist = re_ranking(q_g_dist, q_q_dist, g_g_dist)
+        query = torch.from_numpy(query)
+        test = torch.from_numpy(test)
+        dist = re_ranking(query, test, k1=8, k2=3, lambda_value=0.6)
     else:
+        #dist = cdist(query,test,metric='mahalanobis')
         dist = cosine_similarity(query, test)
+    #dist = normalize(dist, axis=1, norm='l2')
+    #jsonData = json.dumps(dist.tolist())
+    #fileObject = open('./distresult1.json', 'w')
+    #fileObject.write(jsonData)
+    #fileObject.close()
     rank = get_result(dist, queryid, testid)
 
 if __name__ == '__main__':
