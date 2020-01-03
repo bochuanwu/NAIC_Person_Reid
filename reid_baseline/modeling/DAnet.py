c48a3fcb3c006f1fb84659342f755071d0f413a9
bochuan wu <wubochuan@xdf.cn>
2020-01-03 15:29:09 +0800

    add danet and pyramidnet
    设备有限，吊的模型跑不动，拿出来放着了。

---
+++ reid_baseline/modeling/DAnet.py
@@ -0,0 +1,301 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+
+import torch
+from torch import nn
+from torch.nn import Conv2d, Parameter, Softmax
+from .backbones.resnet import *
+from .backbones.senet import SENet, SEResNetBottleneck, SEBottleneck, SEResNeXtBottleneck
+from .backbones.resnet_ibn_a import resnet50_ibn_a
+
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+        nn.init.constant_(m.bias, 0.0)
+    elif classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find('BatchNorm') != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+class Baseline(nn.Module):
+    in_planes = 2048
+
+    def __init__(self, num_classes, last_stride, model_path, neck, neck_feat, model_name, pretrain_choice,with_ibn,gcb,stage_with_gcb):
+        super(Baseline, self).__init__()
+        print(with_ibn)
+        if model_name == 'resnet18':
+            self.in_planes = 512
+            self.base = ResNet.from_name(model_name, last_stride,with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet34':
+            self.in_planes = 512
+            self.base = ResNet.from_name(model_name, last_stride,with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet50':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet101':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet152':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'se_resnet50':
+            self.base = SENet(block=SEResNetBottleneck,
+                              layers=[3, 4, 6, 3],
+                              groups=1,
+                              reduction=16,
+                              dropout_p=None,
+                              inplanes=64,
+                              input_3x3=False,
+                              downsample_kernel_size=1,
+                              downsample_padding=0,
+                              last_stride=last_stride)
+        elif model_name == 'se_resnet101':
+            self.base = SENet(block=SEResNetBottleneck,
+                              layers=[3, 4, 23, 3],
+                              groups=1,
+                              reduction=16,
+                              dropout_p=None,
+                              inplanes=64,
+                              input_3x3=False,
+                              downsample_kernel_size=1,
+                              downsample_padding=0,
+                              last_stride=last_stride)
+        elif model_name == 'se_resnet152':
+            self.base = SENet(block=SEResNetBottleneck,
+                              layers=[3, 8, 36, 3],
+                              groups=1,
+                              reduction=16,
+                              dropout_p=None,
+                              inplanes=64,
+                              input_3x3=False,
+                              downsample_kernel_size=1,
+                              downsample_padding=0,
+                              last_stride=last_stride)
+        elif model_name == 'se_resnext50':
+            self.base = SENet(block=SEResNeXtBottleneck,
+                              layers=[3, 4, 6, 3],
+                              groups=32,
+                              reduction=16,
+                              dropout_p=None,
+                              inplanes=64,
+                              input_3x3=False,
+                              downsample_kernel_size=1,
+                              downsample_padding=0,
+                              last_stride=last_stride)
+        elif model_name == 'se_resnext101':
+            self.base = SENet(block=SEResNeXtBottleneck,
+                              layers=[3, 4, 23, 3],
+                              groups=32,
+                              reduction=16,
+                              dropout_p=None,
+                              inplanes=64,
+                              input_3x3=False,
+                              downsample_kernel_size=1,
+                              downsample_padding=0,
+                              last_stride=last_stride)
+        elif model_name == 'senet154':
+            self.base = SENet(block=SEBottleneck,
+                              layers=[3, 8, 36, 3],
+                              groups=64,
+                              reduction=16,
+                              dropout_p=0.2,
+                              last_stride=last_stride)
+        elif model_name == 'resnet50_ibn_a':
+            self.base = resnet50_ibn_a(last_stride)
+
+        if pretrain_choice == 'imagenet':
+
+            if with_ibn:
+                state_dict = torch.load(model_path)['state_dict']
+                state_dict.pop('module.fc.weight')
+                state_dict.pop('module.fc.bias')
+                new_state_dict = {}
+                for k in state_dict:
+                    new_k = '.'.join(k.split('.')[1:])  # remove module in name
+                    if self.base.state_dict()[new_k].shape == state_dict[k].shape:
+                        new_state_dict[new_k] = state_dict[k]
+                state_dict = new_state_dict
+                self.base.load_state_dict(state_dict, strict=False)
+            else:
+                self.base.load_param(model_path)
+            print('Loading pretrained ImageNet model......')
+
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        # self.gap = nn.AdaptiveMaxPool2d(1)
+        self.num_classes = num_classes
+        self.neck = neck
+        self.neck_feat = neck_feat
+        #PCM CAM
+        self.head = DANetHead(2048, num_classes, nn.BatchNorm2d)
+
+
+        if self.neck == 'no':
+            self.classifier = nn.Linear(self.in_planes, self.num_classes)
+            # self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)     # new add by luo
+            # self.classifier.apply(weights_init_classifier)  # new add by luo
+        elif self.neck == 'bnneck':
+            self.bottleneck = nn.BatchNorm1d(self.in_planes)
+            self.bottleneck.bias.requires_grad_(False)  # no shift
+            self.classifier = nn.Linear(self.in_planes, self.num_classes, bias=False)
+            self.classifier2 = nn.Linear(self.in_planes, self.num_classes, bias=False)
+            self.bottleneck2 = nn.BatchNorm1d(self.in_planes)
+            self.bottleneck2.bias.requires_grad_(False)  # no shift
+            self.bottleneck.apply(weights_init_kaiming)
+            self.bottleneck2.apply(weights_init_kaiming)
+            self.classifier.apply(weights_init_classifier)
+            self.classifier2.apply(weights_init_classifier)
+            # self.classifier = AddMarginProduct(self.in_planes, self.num_classes, s=20, m=0.3)
+
+    def forward(self, x):
+        attention_feat = self.gap(self.head(self.base(x)))
+        global_feat = self.gap(self.base(x))  # (b, 2048, 1, 1)
+        global_feat = global_feat.view(global_feat.shape[0], -1)  # flatten to (bs, 2048)
+        attention_feat = attention_feat.view(attention_feat.shape[0], -1)
+        if self.neck == 'no':
+            feat = global_feat
+        elif self.neck == 'bnneck':
+            feat = self.bottleneck(global_feat)  # normalize for angular softmax
+            feat2 = self.bottleneck2(attention_feat)
+
+        if self.training:
+            cls_score = self.classifier(feat)
+            cls_score2 = self.classifier2(feat2)
+            return [cls_score,cls_score2], [global_feat,attention_feat]  # global feature for triplet loss
+        else:
+            if self.neck_feat == 'after':
+                # print("Test with feature after BN")
+                return torch.cat((feat,feat2),1)
+            else:
+                # print("Test with feature before BN")
+                return global_feat
+
+    def load_param(self, trained_path):
+        param_dict = torch.load(trained_path)
+        for k, v in param_dict.state_dict().items():
+            # print(i)
+            if 'classifier' in k:
+                # print(i[0])
+                continue
+            self.state_dict()[k].copy_(param_dict.state_dict()[k])
+
+
+class DANetHead(nn.Module):
+    def __init__(self, in_channels, out_channels, norm_layer):
+        super(DANetHead, self).__init__()
+        inter_channels = in_channels // 4
+        self.conv5a = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+
+        self.conv5c = nn.Sequential(nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+
+        self.sa = PAM_Module(inter_channels)
+        self.sc = CAM_Module(inter_channels)
+        self.conv51 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+        self.conv52 = nn.Sequential(nn.Conv2d(inter_channels, inter_channels, 3, padding=1, bias=False),
+                                    norm_layer(inter_channels),
+                                    nn.ReLU())
+        self.conv53 = nn.Sequential(nn.Conv2d(inter_channels, 2048, 3, padding=1, bias=False),
+                                    norm_layer(2048),
+                                    nn.ReLU())
+
+
+    def forward(self, x):
+        feat1 = self.conv5a(x)
+        sa_feat = self.sa(feat1)
+        sa_conv = self.conv51(sa_feat)
+
+        feat2 = self.conv5c(x)
+        sc_feat = self.sc(feat2)
+        sc_conv = self.conv52(sc_feat)
+
+        feat_sum = sa_conv + sc_conv
+        feat_sum = self.conv53(feat_sum)
+        return feat_sum
+
+
+
+class PAM_Module(nn.Module):
+    """ Position attention module"""
+    #Ref from SAGAN
+    def __init__(self, in_dim):
+        super(PAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+        self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+        self.gamma = Parameter(torch.zeros(1))
+
+        self.softmax = Softmax(dim=-1)
+    def forward(self, x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X (HxW) X (HxW)
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = self.query_conv(x).view(m_batchsize, -1, width*height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(m_batchsize, -1, width*height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = self.softmax(energy)
+        proj_value = self.value_conv(x).view(m_batchsize, -1, width*height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+        return out
+
+
+class CAM_Module(nn.Module):
+    """ Channel attention module"""
+    def __init__(self, in_dim):
+        super(CAM_Module, self).__init__()
+        self.chanel_in = in_dim
+
+
+        self.gamma = Parameter(torch.zeros(1))
+        self.softmax  = Softmax(dim=-1)
+    def forward(self,x):
+        """
+            inputs :
+                x : input feature maps( B X C X H X W)
+            returns :
+                out : attention value + input feature
+                attention: B X C X C
+        """
+        m_batchsize, C, height, width = x.size()
+        proj_query = x.view(m_batchsize, C, -1)
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1)
+        energy = torch.bmm(proj_query, proj_key)
+        energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
+        attention = self.softmax(energy_new)
+        proj_value = x.view(m_batchsize, C, -1)
+
+        out = torch.bmm(attention, proj_value)
+        out = out.view(m_batchsize, C, height, width)
+
+        out = self.gamma*out + x
+        return out
---
+++ reid_baseline/modeling/pyramidnet.py
@@ -0,0 +1,215 @@
+# encoding: utf-8
+"""
+@author:  liaoxingyu
+@contact: sherlockliao01@gmail.com
+"""
+
+import torch
+from torch import nn
+import copy
+from .backbones.resnet import *
+from .backbones.senet import SENet, SEResNetBottleneck, SEBottleneck, SEResNeXtBottleneck
+from .backbones.resnet_ibn_a import resnet50_ibn_a
+import torch.nn.init as init
+import torch.nn.functional as F
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_out')
+        nn.init.constant_(m.bias, 0.0)
+    elif classname.find('Conv') != -1:
+        nn.init.kaiming_normal_(m.weight, a=0, mode='fan_in')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0.0)
+    elif classname.find('BatchNorm') != -1:
+        if m.affine:
+            nn.init.constant_(m.weight, 1.0)
+            nn.init.constant_(m.bias, 0.0)
+
+
+def weights_init_classifier(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        nn.init.normal_(m.weight, std=0.001)
+        if m.bias:
+            nn.init.constant_(m.bias, 0.0)
+
+class Baseline(nn.Module):
+
+    def __init__(self, num_classes, last_stride, model_path, neck, neck_feat, model_name, pretrain_choice,with_ibn,gcb,stage_with_gcb):
+        super(Baseline, self).__init__()
+        in_planes = 2048
+        num_conv_out_channels = 128
+        global_conv_out_channels = 256
+        num_stripes = 6  # number of sub-parts
+        used_levels = [1, 1, 1, 1, 1, 1]
+        print("num_stripes:{}".format(num_stripes))
+        print("num_conv_out_channels:{},".format(num_conv_out_channels))
+
+        if model_name == 'resnet18':
+            self.in_planes = 512
+            self.base = ResNet.from_name(model_name, last_stride,with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet34':
+            self.in_planes = 512
+            self.base = ResNet.from_name(model_name, last_stride,with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet50':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet101':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+        elif model_name == 'resnet152':
+            self.base = ResNet.from_name(model_name, last_stride, with_ibn, gcb, stage_with_gcb)
+
+
+        if pretrain_choice == 'imagenet':
+
+            if with_ibn:
+                state_dict = torch.load(model_path)['state_dict']
+                state_dict.pop('module.fc.weight')
+                state_dict.pop('module.fc.bias')
+                new_state_dict = {}
+                for k in state_dict:
+                    new_k = '.'.join(k.split('.')[1:])  # remove module in name
+                    if self.base.state_dict()[new_k].shape == state_dict[k].shape:
+                        new_state_dict[new_k] = state_dict[k]
+                state_dict = new_state_dict
+                self.base.load_state_dict(state_dict, strict=False)
+            else:
+                self.base.load_param(model_path)
+            print('Loading pretrained ImageNet model......')
+
+        self.dropout_layer = nn.Dropout(p=0.2)
+        self.num_classes = num_classes
+
+        # ============================================================================== pyramid
+        self.num_stripes = num_stripes
+        self.used_levels = used_levels
+
+        # ==============================================================================pyramid
+        self.pyramid_conv_list0 = nn.ModuleList()
+        self.pyramid_fc_list0 = nn.ModuleList()
+        Baseline.register_basic_branch(self, num_conv_out_channels,
+                                      in_planes,
+                                      self.pyramid_conv_list0,
+                                      self.pyramid_fc_list0)
+
+        # ==============================================================================pyramid
+        input_size = 1024
+        self.pyramid_conv_list1 = nn.ModuleList()
+        self.pyramid_fc_list1 = nn.ModuleList()
+        Baseline.register_basic_branch(self, num_conv_out_channels,
+                                      input_size,
+                                      self.pyramid_conv_list1,
+                                      self.pyramid_fc_list1)
+
+
+    def forward(self, x):
+
+        feat = self.base(x)
+
+        assert feat.size(2) % self.num_stripes == 0
+        # ============================================================================== pyramid
+        feat_list = []
+        logits_list = []
+
+        Baseline.pyramid_forward(self, feat,
+                                self.pyramid_conv_list0,
+                                self.pyramid_fc_list0,
+                                feat_list,
+                                logits_list)
+
+        if self.training:
+            return logits_list, feat_list
+        else:
+            return feat_list
+
+
+    def load_param(self, trained_path):
+        param_dict = torch.load(trained_path)
+        for k, v in param_dict.state_dict().items():
+            # print(i)
+            if 'classifier' in k:
+                # print(i[0])
+                continue
+            self.state_dict()[k].copy_(param_dict.state_dict()[k])
+
+    @staticmethod
+    def register_basic_branch(self, num_conv_out_channels,
+                              input_size,
+                              pyramid_conv_list,
+                              pyramid_fc_list):
+        # the level indexes are defined from fine to coarse,
+        # the branch will contain one more part than that of its previous level
+        # the sliding step is set to 1
+        self.num_in_each_level = [i for i in range(self.num_stripes, 0, -1)]
+        self.num_levels = len(self.num_in_each_level)
+        self.num_branches = sum(self.num_in_each_level)
+
+        idx_levels = 0
+        for idx_branches in range(self.num_branches):
+            if idx_branches >= sum(self.num_in_each_level[0:idx_levels + 1]):
+                idx_levels += 1
+
+            if self.used_levels[idx_levels] == 0:
+                continue
+
+            pyramid_conv_list.append(nn.Sequential(
+                nn.Conv2d(input_size, num_conv_out_channels, 1),
+                nn.BatchNorm2d(num_conv_out_channels),
+                nn.ReLU(inplace=True)))
+
+        # ============================================================================== pyramid
+        idx_levels = 0
+        for idx_branches in range(self.num_branches):
+            if idx_branches >= sum(self.num_in_each_level[0:idx_levels + 1]):
+                idx_levels += 1
+
+            if self.used_levels[idx_levels] == 0:
+                continue
+
+            fc = nn.Linear(num_conv_out_channels, self.num_classes)
+            init.normal_(fc.weight, std=0.001)
+            init.constant_(fc.bias, 0)
+            pyramid_fc_list.append(fc)
+
+    @staticmethod
+    def pyramid_forward(self, feat,
+                        pyramid_conv_list,
+                        pyramid_fc_list,
+                        feat_list,
+                        logits_list):
+
+        basic_stripe_size = int(feat.size(2) / self.num_stripes)
+
+        idx_levels = 0
+        used_branches = 0
+        for idx_branches in range(self.num_branches):
+
+            if idx_branches >= sum(self.num_in_each_level[0:idx_levels + 1]):
+                idx_levels += 1
+
+            if self.used_levels[idx_levels] == 0:
+                continue
+
+            idx_in_each_level = idx_branches - sum(self.num_in_each_level[0:idx_levels])
+
+            stripe_size_in_level = basic_stripe_size * (idx_levels + 1)
+
+            st = idx_in_each_level * basic_stripe_size
+            ed = st + stripe_size_in_level
+
+            local_feat = F.avg_pool2d(feat[:, :, st: ed, :],
+                                      (stripe_size_in_level, feat.size(-1))) + F.max_pool2d(feat[:, :, st: ed, :],
+                                                                                            (stripe_size_in_level,
+                                                                                             feat.size(-1)))
+
+            local_feat = pyramid_conv_list[used_branches](local_feat)
+            local_feat = local_feat.view(local_feat.size(0), -1)
+            feat_list.append(local_feat)
+
+            local_logits = pyramid_fc_list[used_branches](self.dropout_layer(local_feat))
+            logits_list.append(local_logits)
+
+            used_branches += 1
+
+
